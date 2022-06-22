4ec141e946aac3e2846fb57d0d58cd24423a7936
Oceane97440 <sautronoceane98@gmail.com>
2022-06-22 10:22:28 +0400

    calcule dist

--- app/src/main/java/com/example/creolise/ApiAdresse.java
+++ app/src/main/java/com/example/creolise/ApiAdresse.java
@@ -9,6 +9,7 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -23,30 +24,15 @@ public class ApiAdresse extends AppCompatActivity {
         setContentView(R.layout.test);
         RequestQueue requestQueue = Volley.newRequestQueue(this);
 
-        String url = "https://api-adresse.data.gouv.fr/search/?q=reunion&postcode=97420";
+        String url = "https://api-monuments-re.herokuapp.com/monuments?code_postal=97400";
 
-     /*   StringRequest request = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        // traitement en cas de succès
-                        Log.d("api","data "+response);
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                // traitement en cas d'échec
-                Log.d("api","data "+ error);
 
-            }
-        });*/
-
-        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+      /*  JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
                         fillDepartementResultat(response);
-                        //Log.d("api","reponse "+response);
+                        Log.d("api","reponse "+response);
 
                     }
                 }, new Response.ErrorListener() {
@@ -57,46 +43,85 @@ public class ApiAdresse extends AppCompatActivity {
                    Log.d("api","Pas de département trouvé. Veuillez vérifier le code saisi.");
                 } else {
                     Log.d("api","Erreur : " + error.getMessage());
+
                 }
             }
         });
                 requestQueue.add(request);
 
+*/
 
 
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
 
-
-
-
-
-    }
-
-    private void fillDepartementResultat(JSONObject jsonObject) {
-        try {
-            JSONArray features = jsonObject.getJSONArray("features");
-            int length = features.length();
-
-            for (int i = 0; i < length; i++) {
-
-                JSONObject data = features.getJSONObject(i);
-                JSONObject geometry = data.getJSONObject("geometry");
-                JSONArray coordinates = geometry.getJSONArray("coordinates");
-
-                JSONObject properties = data.getJSONObject("properties");
-                String adresse = properties.getString("label");
-                String postcode = properties.getString("postcode");
-                String citycode = properties.getString("citycode");
-
-                Log.d("api","data "+coordinates.get(0)+" - "+coordinates.get(1)+" - "+adresse
-                +" -" +postcode+" -" +citycode
-                );
-
+                AllDataRep(response);
 
             }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                if(error.networkResponse != null
+                        && error.networkResponse.statusCode == 404) {
+                    Log.d("api","Pas de département trouvé. Veuillez vérifier le code saisi.");
+                } else {
+                    Log.d("api","Erreur : " + error.getMessage());
+
+                }
+            }
+        });
+
+                requestQueue.add(jsonArrayRequest);
+
+    }
+
+    private void AllDataRep(JSONArray response) {
+        for (int i = 0; i < response.length(); i++) {
+
+            try {
+                // we are getting each json object.
+                JSONObject responseObj = response.getJSONObject(i);
 
 
-        } catch (JSONException e) {
-            Log.d("api","Erreur : " + e.getMessage());
+                // similarly we are extracting all the strings from our json object.
+                String code_postal = responseObj.getString("code_postal");
+                String ville = responseObj.getString("ville");
+
+                JSONArray lieux_historique = responseObj.getJSONArray("lieux_historique");
+                int length = lieux_historique.length();
+
+                for (int d = 0; i < length; d++) {
+
+                    JSONObject data = lieux_historique.getJSONObject(d);
+
+                    JSONArray coordinates = data.getJSONArray("coordonnees_finales");
+
+                    String historique = data.getString("historique");
+
+                   // String adresse = data.getString("adresse");
+
+                    String nom = data.getString("appellation_courante");
+
+                    double coordinates_latitude = (double) coordinates.get(0);
+                    double coordinates_longitude = (double) coordinates.get(1);
+
+                    Log.d("api","data "+code_postal+ville+historique+nom+coordinates_longitude+coordinates_latitude);
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
     }
+
+   /* private void fillDepartementResultat(JSONObject jsonObject) {
+
+       // Log.d("api","-"+jsonObject);
+
+
+    }*/
+
+
+
 }
--- app/src/main/java/com/example/creolise/Location.java
+++ app/src/main/java/com/example/creolise/Location.java
@@ -1,9 +1,7 @@
 package com.example.creolise;
 
 import android.Manifest;
-import android.app.SearchManager;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
@@ -19,8 +17,20 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.util.List;
+import java.util.Locale;
 
 public class Location extends AppCompatActivity implements LocationListener {
 
@@ -55,14 +65,14 @@ public class Location extends AppCompatActivity implements LocationListener {
     @Override
     public void onLocationChanged(@NonNull android.location.Location location) {
 
-        Log.d("gps", location.getLatitude() + " - " + location.getLongitude());
+      //  Log.d("gps", location.getLatitude() + " - " + location.getLongitude());
         //convertie les données lat et log en adresse
         Geocoder geocoder = new Geocoder(this);
 
         try {
             List<Address> addresses = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(), 1);
 
-            Log.d("gps","data "+addresses.get(0).getAddressLine(0));
+           String codePostal = addresses.get(0).getPostalCode();
             System.out.println(addresses.get(0).getAddressLine(0));
 
             //recupère UI textView
@@ -73,14 +83,139 @@ public class Location extends AppCompatActivity implements LocationListener {
             //Insert les coordonnée gps dans les ui
             latText.setText(location.getLatitude()+"");
             logText.setText(location.getLongitude()+"");
-
             addrTxt.setText(addresses.get(0).getAddressLine(0));
+
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+            /*String url = "https://api-adresse.data.gouv.fr/search/?q=reunion&postcode="+codePostal;
+
+
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            fillDepartementResultat(response ,location);
+                            //Log.d("api","reponse "+response);
+
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    if(error.networkResponse != null
+                            && error.networkResponse.statusCode == 404) {
+                        Log.d("api","Pas de département trouvé. Veuillez vérifier le code saisi.");
+                    } else {
+                        Log.d("api","Erreur : " + error.getMessage());
+                    }
+                }
+            });
+            requestQueue.add(request);*/
+
+            String url = "https://api-monuments-re.herokuapp.com/monuments?code_postal="+codePostal;
+
+            JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+                @Override
+                public void onResponse(JSONArray response) {
+
+                    AllDataRep(response,location);
+
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    if(error.networkResponse != null
+                            && error.networkResponse.statusCode == 404) {
+                        Log.d("api","Pas de département trouvé. Veuillez vérifier le code saisi.");
+                    } else {
+                        Log.d("api","Erreur : " + error.getMessage());
+
+                    }
+                }
+            });
+
+            requestQueue.add(jsonArrayRequest);
+
 
 
 
         }catch (IOException e){
             e.printStackTrace();
         }
+
+    }
+
+    private void AllDataRep(JSONArray response, android.location.Location location) {
+        double latitude = location.getLatitude();
+         double longitude =  location.getLongitude();
+
+
+            try {
+                for (int i = 0; i < response.length(); i++) {
+
+                    // we are getting each json object.
+                    JSONObject responseObj = response.getJSONObject(i);
+                    // similarly we are extracting all the strings from our json object.
+                    String code_postal = responseObj.getString("code_postal");
+                    String ville = responseObj.getString("ville");
+
+                    JSONArray lieux_historique = responseObj.getJSONArray("lieux_historique");
+                    int length = lieux_historique.length();
+
+                    for (int d = 0; i < length; d++) {
+
+                        JSONObject data = lieux_historique.getJSONObject(d);
+
+                        JSONArray coordinates = data.getJSONArray("coordonnees_finales");
+                        String historique = data.getString("historique");
+                        String nom = data.getString("appellation_courante");
+                         double coordinates_latitude = (double) coordinates.get(0);
+                         double coordinates_longitude = (double)coordinates.get(1);
+
+
+                        distance(latitude, longitude, coordinates_latitude,
+                                coordinates_longitude,nom,code_postal,ville,historique);
+
+
+                           // Log.d("api", "data " +nom + coordinates_longitude + coordinates_latitude);
+
+                        }
+
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+    }
+
+    private void distance(double latitude, double longitude, double endLatitude, double endLongitude, String nom, String code_postal, String ville, String historique) {
+
+        double theta = longitude - endLongitude;
+        double dist = Math.sin(deg2rad(latitude))
+                * Math.sin(deg2rad(endLatitude))
+                + Math.cos(deg2rad(latitude))
+                * Math.cos(deg2rad(endLatitude))
+                * Math.cos(deg2rad(theta));
+        dist = Math.acos(dist);
+        dist = rad2deg(dist);
+        dist = dist * 60 * 1.1515;
+
+
+        if (dist<=2){
+            Log.d("gps","test distance "+String.format(Locale.FRENCH,"%,f",dist));
+
+            Log.d("api", "data " +nom + endLatitude + endLongitude);
+
+        }
+
+    }
+
+    private double rad2deg(double dist) {
+        return (dist * 180.0 / Math.PI);
+
+    }
+
+    private double deg2rad(double latitude) {
+        return (latitude * Math.PI / 180.0);
 
     }
 
@@ -107,4 +242,6 @@ public class Location extends AppCompatActivity implements LocationListener {
             }
         }
     }
+
+
 }
